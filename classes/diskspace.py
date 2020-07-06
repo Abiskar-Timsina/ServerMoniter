cb50058e13c9647b1e80334122a1b3b1e09f5737
Abiskar-Timsina <abiskartimsina4@gmail.com>
2020-07-06 19:55:19 +0545

    Added comments

--- classes/diskspace.py
+++ classes/diskspace.py
@@ -10,51 +10,74 @@ class Disk(object):
     graph_usuage = []
     graph_day = []
     drive_info = {}
-    password = #<Your login password for the associated gmail>
+    password = #<Your password for the associated gmail>
     
+
+    # this runs the df -h command on terminal and pipes the output to a temp file
     @classmethod
     def read_sys(cls):
         with open('./DiskUsuage.txt', 'w') as f:
-            file_data = subprocess.run(
-                'df -h', shell=True, text=True, stdout=f)
+            file_data = subprocess.run('df -h', shell=True, text=True, stdout=f)
 
+
+    # this appends all the relavent info to the drive_info dictionary which is used to retrive total/used space in the main.py file
     @classmethod        
     def ret_fun(cls,**kwargs):
         for arg,value in kwargs.items():
             cls.drive_info.update({arg:value})
 
 
+    # this method read the output for the read_sys file where each line corresponds to a drive 
     @classmethod
     def read_data(cls):
+
         with open('./DiskUsuage.txt', 'r') as f:
             a = f.readlines()
 
         check = len(a)
 
         for i in range(check):
-            i += 3
+            i += 3 # 3 because df -h returns the output for temp folders which in this case is not required (for me)
 
-            drive = a[i][0:9] #0:2 for windows
+            drive = a[i][0:9] #0:2 for windows # Parsing throught the string to get only  drive name
  
-            total = int(a[i][16:19]) #16:19 for windows
+            total = int(a[i][16:19]) #16:19 for windows #Parsing throught the string to get only the total memory allocated
 
-            used = float(a[i][22:25]) #23:25 for windows
+            used = float(a[i][22:25]) #23:25 for windows #Parsing throught the string to get only the used memory
 
-            day = time.localtime().tm_yday
+            day = time.localtime().tm_yday 
 
+            '''
+            Year day over month day because at the start of each month the x-axis becomes 0; this causes a hard to read graph
+
+            A possible and simple solution is to make multiple graphs and append them to a single plot to differentiate the different months
+            which will probably be added in upcoming versions
+            '''
+
+            # appending to graph_day / graph_usuage so that it can reatain info about the current state whenever it is called.
             cls.graph_day.append(day)
             cls.graph_usuage.append((used))
             # print(f'Drive: {drive}, Total Space: {total} GB ,Used Space: {used} GB, Available: {total - used} GB, % Used: {int((lambda : (used/total)*100)())} %')
             cls.ret_fun(drive=drive,total=total,used=used)
             break
 
+    '''
+    the info.config file contains a single array in the format of;
+    [<day>,<space used>]
 
+    which over the time increases as;
 
+    [[<day>,<space used>],[<day1>,<space used1>],[<day2>,<space used2>],[<day3>,<space used3>]]
+    '''
+
+    # this is used to update the array stored in the info.config file so that the space used and day/time are retrived and plotted as necessary
     @classmethod
     def update(cls, ar):
         with open('./info.config', 'wb') as f:
             pickle.dump(ar, f)
 
+
+    #checks the previous saved data and add it to the graph_day,graph_usuage list to be plotted again
     @classmethod
     def check_prev(cls):
 	    if(os.path.exists('./info.config')):
@@ -66,6 +89,7 @@ class Disk(object):
 	        for u in data[1]:
 	            cls.graph_usuage.append(u)
 
+    # this method plots the graph using matplotlib 
     @classmethod
     def save_data(cls):
         plt.style.use('dark_background')
@@ -78,10 +102,12 @@ class Disk(object):
         ar = [cls.graph_day, cls.graph_usuage]
         cls.update(ar)
 
+    # this method clears the temp file
     @classmethod
     def clear_mem(cls):
         os.remove('./DiskUsuage.txt')
 
+    # this method makes it easier to run each of the methods of the class in order.
     @classmethod
     def main_run(cls):
        cls.read_sys()       
--- main.py
+++ main.py
@@ -8,28 +8,41 @@ from classes.diskspace import Disk
 def main():
 	msg = EmailMessage()
 	msg['Subject'] = 'Server Status'
-	msg['From'] = #<Your Email Address>
-	msg['To'] = #<Target Email Address>
+	msg['From'] = 'abiskartimsina4mini@gmail.com'
+	msg['To'] = 'abiskartimsina4@gmail.com'
+	email = msg['From']
 
-	obj = Disk()
-	obj.main_run()
-	data = obj.drive_info
+	#creating an object for Disk class
+	disk_object = Disk()
+	disk_object.main_run()
+	disk_info = obj.drive_info
 
+	#retriving relavent info 
+	drive,total,used = disk_info['drive'],disk_info['total'],disk_info['used']
 
-	drive = data['drive']
-	total=data['total']
-	used=data['used']
-
+	#formatting the type of message to be sent
 	message = f'Disk Usuage of the Server Drive : {drive}\nTotal: {total}GB\nUsed:{used}GB'
-	with open('Graph1.png','rb') as f:
+	
+	#opening the the graph
+	with open('Graph.png','rb') as f:
 		data = f.read()
 
-	with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
-		smtp.login('<Your Email Address>', Disk().password)
+	#sending the email
 
+	#465 is the general smtp port
+	#smtp.gmail.com is the service for gmail
+
+	with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
+		# email as defined earlier, .password is an attribute of the Disk class that just stores the password 
+		smtp.login(email, Disk().password)
+
+		#set the body of the message
 		msg.set_content(message)
+
+		#adding the image of the graph as attachment
 		msg.add_attachment(data,maintype='image',subtype='png',filename='graph')
 		
+		#sending the final message
 		smtp.send_message(msg)
 
 if __name__ == '__main__':
